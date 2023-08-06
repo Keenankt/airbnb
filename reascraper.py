4ebd508378756879899f19d133be6811877367bb
Keenan <kkuklisatunhla@gmail.com>
2023-08-06 21:07:24 +1000

    commit

--- reascraper.py
+++ reascraper.py
@@ -3,8 +3,10 @@ from bs4 import BeautifulSoup as soup
 import pandas as pd
 import numpy as np
 
+#initialise webdriver
 driver = webdriver.Chrome()
 
+#initialise frames
 prices = []
 beds = []
 baths = []
@@ -13,53 +15,58 @@ types = []
 addresses1 = []
 addresses2 = []
 
-driver.get('https://www.domain.com.au/rent/?postcode=3006&excludedeposittaken=1&page=1')
-webpage = driver.page_source
-parsed_webpage = soup(webpage,features='html.parser')    
-total_listings = parsed_webpage.find('h1', attrs={'class': 'css-ekkwk0'}).get_text() 
-total_listings = total_listings.split()
-total_listings = int(total_listings[0])
-pages_to_search = int(round(total_listings / 20))
+#read postcode list
+postcodes = pd.read_csv('vic_postcodes.csv')
+postcodes = postcodes['postcode']
 
-for page in range(1,pages_to_search+1):
-    driver.get('https://www.domain.com.au/rent/?postcode=3006&excludedeposittaken=1&page=' + str(page))
-
+for postcode in postcodes:
+    driver.get('https://www.domain.com.au/rent/?postcode=' + str(postcode) + '&excludedeposittaken=1&page=1')
     webpage = driver.page_source
+    parsed_webpage = soup(webpage,features='html.parser')    
+    total_listings = parsed_webpage.find('h1', attrs={'class': 'css-ekkwk0'}).get_text() 
+    total_listings = total_listings.split()
+    total_listings = int(total_listings[0])
+    pages_to_search = int(round(total_listings / 20))
 
-    parsed_webpage = soup(webpage,features='html.parser')
+    for page in range(1,pages_to_search+1):
+        driver.get('https://www.domain.com.au/rent/?postcode=' + str(postcode) + '&excludedeposittaken=1&page=' + str(page))
 
-    for element in parsed_webpage.findAll('li', attrs={'class': 'css-1qp9106'}):
-        price = element.find('p', attrs={'data-testid': 'listing-card-price'})
-        prices.append(price.get_text())
+        webpage = driver.page_source
 
-        features = element.findAll('span', attrs={'data-testid': 'property-features-text-container'}, limit=3) #not great but good enough
-        if features != None:     
-            bed, bath, car = features
-            beds.append(bed.get_text())
-            baths.append(bath.get_text())
-            cars.append(car.get_text())
-        else:
-            beds.append("Unknown")
-            baths.append("Unknown")
-            cars.append("Unknown") 
+        parsed_webpage = soup(webpage,features='html.parser')
 
-        type = element.find('div', attrs={'class': 'css-11n8uyu'})
-        if type != None:
-            types.append(type.get_text())
-        else:
-            types.append('Unknown')
+        for element in parsed_webpage.findAll('li', attrs={'class': 'css-1qp9106'}):
+            price = element.find('p', attrs={'data-testid': 'listing-card-price'})
+            prices.append(price.get_text())
 
-        address1 = element.find('span', attrs={'data-testid': 'address-line1'})
-        if address1 != None:
-            addresses1.append(address1.get_text())
-        else:
-            addresses1.append('Unknown')
+            features = element.findAll('span', attrs={'data-testid': 'property-features-text-container'}, limit=3) #not great but good enough
+            if len(features) == 3:     
+                bed, bath, car = features
+                beds.append(bed.get_text())
+                baths.append(bath.get_text())
+                cars.append(car.get_text())
+            else:
+                beds.append("Unknown")
+                baths.append("Unknown")
+                cars.append("Unknown") 
 
-        address2 = element.find('span', attrs={'data-testid': 'address-line2'})
-        if address2 != None:
-            addresses2.append(address2.get_text())
-        else:
-            addresses2.append('Unknown')
+            type = element.find('div', attrs={'class': 'css-11n8uyu'})
+            if type != None:
+                types.append(type.get_text())
+            else:
+                types.append('Unknown')
+
+            address1 = element.find('span', attrs={'data-testid': 'address-line1'})
+            if address1 != None:
+                addresses1.append(address1.get_text())
+            else:
+                addresses1.append('Unknown')
+
+            address2 = element.find('span', attrs={'data-testid': 'address-line2'})
+            if address2 != None:
+                addresses2.append(address2.get_text())
+            else:
+                addresses2.append('Unknown')
 
 
 print(prices)
