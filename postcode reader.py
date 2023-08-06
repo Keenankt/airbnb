a12363cb47dd9c6e7f084bfdffc2a73548d79f5b
Keenan <kkuklisatunhla@gmail.com>
2023-08-06 13:37:59 +1000

    commit

--- postcode reader.py
+++ postcode reader.py
@@ -4,8 +4,12 @@ import numpy as np
 postcodes = pd.read_csv('au_postcodes.csv')
 
 
-vic_postcodes = postcodes[postcodes['postcode'].astype(str).str.startswith('3')]
+#vic_postcodes = postcodes[postcodes['postcode'].astype(str).str.startswith('3')]
+#vic_postcodes = postcodes[postcodes['state_code']=="VIC"]
+
+vic_postcodes = postcodes[(postcodes['state_code']=="VIC") & postcodes['postcode'].astype(str).str.startswith('3')]
 
 pd.DataFrame.to_csv(vic_postcodes,"vic_postcodes.csv")
+
 print(vic_postcodes)
 
