a78e56f9eb1c43d55e67b58f352dd79572c57410
Keenankt <139690815+Keenankt@users.noreply.github.com>
2023-08-25 19:34:46 +1000

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-## import packages 
-import pandas as pd
-import numpy as np
-import geopy
-
-df = pd.read_csv('airbnb_listings_clean.csv')
-df['postcode'].replace(to_replace='Not Found', value=0000,inplace=True)
-df.to_csv('airbnb_listings_clean.csv', index=False, encoding='utf-8')
